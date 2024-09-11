6c6893f9371b4af115ef635fe997e7bfe4d4f04f
YuQiJaang <rdjksp@gmail.com>
2024-09-12 00:26:29 +0800

    添加原始翻译功能

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use crate::cli::cli_handle::CliParsedWay;
 
 mod cli;
 mod config_save_and_load;
@@ -7,23 +8,22 @@ mod sdk;
 #[tokio::main]
 async fn main() {
     let sdk = sdk::caller::TencentCloudTranslateSDK::from_env().expect("SDK Key未配置！请配置后使用");
-    let s = sdk.translate_text("Hello", &"en".to_string(), &"zh".to_string()).await.unwrap();
-    println!("{}", s);
-    // match cli::cli_handle::CliHandler::parse().user_to_do() {
-    //     CliParsedWay::AddConfig(_) => {
-    //         println!("AddConfig command not implemented yet");
-    //     }
-    //     CliParsedWay::DelConfig(_) => {
-    //         println!("DelConfig command not implemented yet");
-    //     }
-    //     CliParsedWay::TranslateWithParam(_, _, _) => {
-    //         println!("TranslateWithParam command not implemented yet");
-    //     }
-    //     CliParsedWay::TranslateWithConfig(_, _) => {
-    //         println!("TranslateWithConfig command not implemented yet");
-    //     }
-    //     CliParsedWay::Unknown => {
-    //         panic!("未知的命令形式！输入-help查看命令");
-    //     }
-    // }
+
+    match cli::cli_handle::CliHandler::parse().user_to_do() {
+        CliParsedWay::AddConfig(_) => {
+            println!("AddConfig command not implemented yet");
+        }
+        CliParsedWay::DelConfig(_) => {
+            println!("DelConfig command not implemented yet");
+        }
+        CliParsedWay::TranslateWithParam(p, from, target) => {
+            print!("{}", sdk.translate_text(&p, &from, &target));
+        }
+        CliParsedWay::TranslateWithConfig(_, _) => {
+            println!("TranslateWithConfig command not implemented yet");
+        }
+        CliParsedWay::Unknown => {
+            panic!("未知的命令形式！输入-help查看命令");
+        }
+    }
 }
